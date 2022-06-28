e3e195ee29b46e6b04861f7562a8f1aad5ef887d
Katrin3744 <k.kukh3477@gmail.com>
2022-06-29 01:04:32 +0300

    Added bootstrap CDN

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,16 @@ ActiveRecord::Schema.define(version: 2022_06_28_181049) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "answers", force: :cascade do |t|
+    t.text "body", null: false
+    t.bigint "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_answers_on_author_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
   create_table "comments", force: :cascade do |t|
     t.string "body", null: false
     t.integer "author_id", null: false
@@ -34,6 +44,15 @@ ActiveRecord::Schema.define(version: 2022_06_28_181049) do
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
+  create_table "questions", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id"
+    t.index ["author_id"], name: "index_questions_on_author_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "login", null: false
     t.boolean "is_author", default: false
@@ -48,6 +67,7 @@ ActiveRecord::Schema.define(version: 2022_06_28_181049) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "answers", "questions"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
